60a623d7eccee98376d68391315d0836b1d33ac6
RamadhantyDwiAgustia <ramadhantydwiagustia@gmail.com>
2020-08-02 13:15:16 +0800

    make resource

--- app/Model/MemberFitness.php
+++ app/Model/MemberFitness.php
@@ -3,8 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class MemberFitness extends Model
 {
     //
+    use Notifiable;
+
+    protected $fillable = [
+        'nama_member','no_hp','alamat','kode_boking','paket_member','paket_fitnes','password'
+    ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nama_member');
+            $table->string('no_hp');
+            $table->string('alamat');
+            $table->string('kode_booking');
+            $table->string('paket_member');
+            $table->string('paket_fitness');
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
